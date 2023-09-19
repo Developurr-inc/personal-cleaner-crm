61b938682c664d2d6a0ee7e36c5e867ab90d00b4
Vinícius Ferreira Aguiar <acevinicius@icloud.com>
2023-09-19 11:58:39 -0300

    Update CustomerId.cs
    Signed-off-by: Vinícius Ferreira Aguiar <acevinicius@icloud.com>

--- src/Orderly.Domain/Customer/ValueObjects/CustomerId.cs
+++ src/Orderly.Domain/Customer/ValueObjects/CustomerId.cs
@@ -1,21 +1,16 @@
 using Orderly.Domain.SeedWork;
-
 namespace Orderly.Domain.Customer.ValueObjects;
-
 public sealed class CustomerId : ValueObject
 {
     public Guid Value { get; }
-
     private CustomerId()
     {
         Value = Guid.NewGuid();
     }
-
     public static CustomerId Create()
     {
         return new CustomerId();
     }
-
     protected override IEnumerable<object> GetEqualityComponents()
     {
         yield return Value;
