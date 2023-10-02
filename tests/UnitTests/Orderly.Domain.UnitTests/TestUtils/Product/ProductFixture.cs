400871c4f84092e3e0f6a01f2ddddf1653633c4a
Guilherme Kneitz <guikneitz@gmail.com>
2023-10-02 14:23:04 -0300

    fixed Product tests

--- tests/UnitTests/Orderly.Domain.UnitTests/TestUtils/Product/ProductFixture.cs
+++ tests/UnitTests/Orderly.Domain.UnitTests/TestUtils/Product/ProductFixture.cs
@@ -5,47 +5,15 @@ using Orderly.Domain.UnitTests.TestUtils.String;
 
 namespace Orderly.Domain.UnitTests.TestUtils.Product;
 
-public sealed class ProductFixture : BaseFixture
+public static class ProductFixture
 {
-    private static Domain.Product.Product CreateValidProduct()
+    public static Domain.Product.Product CreateProduct()
     {
-        var price = Faker.Random.Decimal(0, 100);
-        var code = StringFixture.CreateString(5, 255);
-        var packaging = StringFixture.CreateString(5, 255);
-        var exciseTax = Faker.Random.Decimal(0, 100);
-
-        var name = StringFixture.CreateString(
-            ProductValidatorConfig.NameMinLength,
-            ProductValidatorConfig.NameMaxLength
-        );
-
-        return Domain.Product.Product.Create(code, name, packaging, exciseTax, price);
-    }
-
-    public static Domain.Product.Product CreateProduct(
-        Domain.Product.Product? product = null,
-        string? name = null,
-        decimal? price = null
-    )
-    {
-        var lProduct = product ?? CreateValidProduct();
-
         return Domain.Product.Product.Create(
-            lProduct.Code,
-            name ?? lProduct.Name,
-            lProduct.Packaging,
-            lProduct.ExciseTax,
-            price ?? lProduct.Price.Value
-        );
-    }
-
-    public static string CreateShortName()
-    {
-        return StringFixture.CreateString(1, ProductValidatorConfig.NameMinLength - 1);
-    }
-
-    public static string CreateLongName()
-    {
-        return StringFixture.CreateString(ProductValidatorConfig.NameMaxLength + 1, 1_000);
+            Constants.Constants.Product.Code,
+            Constants.Constants.Product.Name,
+            Constants.Constants.Product.Packaging,
+            Constants.Constants.Product.ExciseTax,
+            Constants.Constants.Product.PriceValue);
     }
 }
