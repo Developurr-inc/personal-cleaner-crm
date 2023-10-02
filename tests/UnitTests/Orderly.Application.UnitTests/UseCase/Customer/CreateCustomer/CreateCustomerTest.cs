87c9de4692b821bef22bc156caaa82ed80195804
Vinicius Aguiar <acevinicius@icloud.com>
2023-10-02 15:58:05 -0300

    More examples

--- tests/UnitTests/Orderly.Application.UnitTests/UseCase/Customer/CreateCustomer/CreateCustomerTest.cs
+++ tests/UnitTests/Orderly.Application.UnitTests/UseCase/Customer/CreateCustomer/CreateCustomerTest.cs
@@ -10,8 +10,7 @@ namespace Orderly.Application.UnitTests.UseCase.Customer.CreateCustomer;
 public class CreateCustomerTest
 {
     [Fact]
-    public void
-        GivenValidInput_WhenInstantiatingCreateCustomerUseCase_ThenShouldInstantiateCreateCustomerUseCase()
+    public void GivenValidInput_WhenInstantiatingCreateCustomerUseCase_ThenShouldInstantiateCreateCustomerUseCase()
     {
         // Arrange
         var unitOfWorkMock = new Mock<IUnitOfWork>();
@@ -26,8 +25,41 @@ public class CreateCustomerTest
     }
     
     [Fact]
-    public async void
-        GivenValidInput_WhenCallingExecute_ThenShouldCallCommitAsyncOnce()
+    public async void GivenValidInput_WhenCallingExecute_ThenShouldCallGetByIdAsyncOnce()
+    {
+        // Arrange
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
+        var customerRepositoryMock = new Mock<ICustomerRepository>();
+        var salesConsultantRepositoryMock = new Mock<ISalesConsultantRepository>();
+        var createCustomerUseCase = new CreateCustomerUseCase(unitOfWorkMock.Object, customerRepositoryMock.Object, salesConsultantRepositoryMock.Object);
+        var input = CreateCustomerFixture.CreateInput();
+
+        // Act
+        _ = await createCustomerUseCase.Execute(input, CancellationToken.None);
+        
+        // Assert
+        salesConsultantRepositoryMock.Verify(x => x.GetByIdAsync(It.IsAny<string>(), CancellationToken.None), Times.Once);
+    }
+
+    [Fact]
+    public async void GivenValidInput_WhenCallingExecute_ThenShouldCallInsertAsyncOnce()
+    {
+        // Arrange
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
+        var customerRepositoryMock = new Mock<ICustomerRepository>();
+        var salesConsultantRepositoryMock = new Mock<ISalesConsultantRepository>();
+        var createCustomerUseCase = new CreateCustomerUseCase(unitOfWorkMock.Object, customerRepositoryMock.Object, salesConsultantRepositoryMock.Object);
+        var input = CreateCustomerFixture.CreateInput();
+
+        // Act
+        _ = await createCustomerUseCase.Execute(input, CancellationToken.None);
+        
+        // Assert
+        customerRepositoryMock.Verify(x => x.InsertAsync(It.IsAny<Domain.Customer.Customer>(), CancellationToken.None), Times.Once);
+    }
+    
+    [Fact]
+    public async void GivenValidInput_WhenCallingExecute_ThenShouldCallCommitAsyncOnce()
     {
         // Arrange
         var unitOfWorkMock = new Mock<IUnitOfWork>();
@@ -42,46 +74,9 @@ public class CreateCustomerTest
         // Assert
         unitOfWorkMock.Verify(x => x.CommitAsync(CancellationToken.None), Times.Once);
     }
-    
-    [Fact]
-    public async void
-        GivenValidInput_WhenCallingExecute_ThenShouldCallInsertAsyncOnce()
-    {
-        // Arrange
-        var unitOfWorkMock = new Mock<IUnitOfWork>();
-        var customerRepositoryMock = new Mock<ICustomerRepository>();
-        var salesConsultantRepositoryMock = new Mock<ISalesConsultantRepository>();
-        var createCustomerUseCase = new CreateCustomerUseCase(unitOfWorkMock.Object, customerRepositoryMock.Object, salesConsultantRepositoryMock.Object);
-        var input = CreateCustomerFixture.CreateInput();
-        
-        // Act
-        _ = await createCustomerUseCase.Execute(input, CancellationToken.None);
-        
-        // Assert
-        customerRepositoryMock.Verify(x => x.InsertAsync(It.IsAny<Domain.Customer.Customer>(), CancellationToken.None), Times.Once);
-    }
-    
-    [Fact]
-    public async void
-        GivenValidInput_WhenCallingExecute_ThenShouldCallGetByIdAsyncOnce()
-    {
-        // Arrange
-        var unitOfWorkMock = new Mock<IUnitOfWork>();
-        var customerRepositoryMock = new Mock<ICustomerRepository>();
-        var salesConsultantRepositoryMock = new Mock<ISalesConsultantRepository>();
-        var createCustomerUseCase = new CreateCustomerUseCase(unitOfWorkMock.Object, customerRepositoryMock.Object, salesConsultantRepositoryMock.Object);
-        var input = CreateCustomerFixture.CreateInput();
-        
-        // Act
-        _ = await createCustomerUseCase.Execute(input, CancellationToken.None);
-        
-        // Assert
-        salesConsultantRepositoryMock.Verify(x => x.GetByIdAsync(It.IsAny<string>(), CancellationToken.None), Times.Once);
-    }
 
     [Fact]
-    public async void
-        GivenValidInput_WhenCallingExecute_ThenShouldReturnValidOutput()
+    public async void GivenValidInput_WhenCallingExecute_ThenShouldReturnValidOutput()
     {
         // Arrange
         var createCustomerUseCase = CreateCustomerFixture.CreateUseCase();
@@ -95,18 +90,79 @@ public class CreateCustomerTest
     }
     
     [Fact]
-    public async void
-        GivenValidInput_WhenCallingExecute_ThenShouldReturnOutput()
+    public async void GivenValidInput_WhenCallingExecute_ThenShouldReturnOutput()
     {
         // Arrange
         var createCustomerUseCase = CreateCustomerFixture.CreateUseCase();
         var input = CreateCustomerFixture.CreateInput();
-        var expectedOutput = CreateCustomerFixture.CreateOutput();
 
         // Act
         var output = await createCustomerUseCase.Execute(input, CancellationToken.None);
 
         // Assert
-        Assert.Equal(expectedOutput, output);
+        Assert.NotEmpty(output.CustomerId);
+    }
+
+    [Fact]
+    public async void GivenInvalidSalesConsultantId_WhenCallingExecute_ThenShouldReturnNotFoundExceptionWithMessage()
+    {
+        // Arrange
+        var createCustomerUseCase = CreateCustomerFixture.CreateUseCase();
+        var input = CreateCustomerFixture.CreateInput();
+        const string expectedMessage = "aaa";
+
+        // Act
+        var exception = await Record.ExceptionAsync(() => createCustomerUseCase.Execute(input, CancellationToken.None));
+
+        // Assert
+        var notFoundException = Assert.IsType<NotFoundException>(exception);
+        Assert.Equal(expectedMessage, notFoundException.Message);
+    }
+
+    [Fact]
+    public async void GivenInvalidInput_WhenCallingExecute_ThenShouldReturnDomainValidationExceptionWithMessage()
+    {
+        // Arrange
+        var createCustomerUseCase = CreateCustomerFixture.CreateUseCase();
+        var input = CreateCustomerFixture.CreateInvalidInput();
+
+        // Act
+        var exception = await Record.ExceptionAsync(() => createCustomerUseCase.Execute(input, CancellationToken.None));
+
+        // Assert
+        var domainValidationException = Assert.IsType<DomainValidationException>(exception);
+        Assert.NotEmpty(domainValidationException.Errors);
+    }
+
+    [Fact]
+    public async void GivenRepositoryError_WhenCallingExecute_ThenShouldReturnRepositoryExceptionWithMessage()
+    {
+        // Arrange
+        var createCustomerUseCase = CreateCustomerFixture.CreateUseCase();
+        var input = CreateCustomerFixture.CreateInput();
+        const string expectedMessage = "aaa";
+
+        // Act
+        var exception = await Record.ExceptionAsync(() => createCustomerUseCase.Execute(input, CancellationToken.None));
+
+        // Assert
+        var repositoryException = Assert.IsType<RepositoryException>(exception);
+        Assert.Equal(expectedMessage, repositoryException.Message);
+    }
+
+    [Fact]
+    public void GivenUnitOfWorkError_WhenCallingExecute_ThenShouldReturnUnitOfWorkExceptionWithMessage()
+    {
+        // Arrange
+        var createCustomerUseCase = CreateCustomerFixture.CreateUseCase();
+        var input = CreateCustomerFixture.CreateInput();
+        const string expectedMessage = "aaa";
+
+        // Act
+        var exception = Record.ExceptionAsync(() => createCustomerUseCase.Execute(input, CancellationToken.None));
+
+        // Assert
+        var unitOfWorkException = Assert.IsType<UnitOfWorkException>(exception);
+        Assert.Equal(expectedMessage, unitOfWorkException.Message);
     }
 }
