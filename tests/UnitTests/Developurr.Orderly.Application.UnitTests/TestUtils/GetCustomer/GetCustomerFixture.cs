97f79e7129e9fc53d047bff4e1a247e4e6e4809f
Dudu Seabra <duduseabra99@gmail.com>
2023-10-17 17:46:11 -0300

    Add GetCustomerFixture.cs and GetCustomerTest.cs

---
+++ tests/UnitTests/Developurr.Orderly.Application.UnitTests/TestUtils/GetCustomer/GetCustomerFixture.cs
@@ -0,0 +1,25 @@
+using Developurr.Orderly.Application.Query.Customer.GetCustomer;
+using Developurr.Orderly.Domain.Customer.Repositories;
+using Developurr.Orderly.Domain.UnitTests.TestUtils.Customer;
+using Moq;
+
+namespace Developurr.Orderly.Application.UnitTests.TestUtils.GetCustomer;
+
+public static class GetCustomerFixture
+{
+    public static GetCustomerUseCase CreateUseCase()
+    {
+        var customerRepositoryMock = new Mock<ICustomerRepository>();
+        var input = CreateInput();
+        var customer = CustomerFixture.CreateCustomer();
+        
+        customerRepositoryMock.Setup(x => x.GetByIdAsync(input.CustomerId, It.IsAny<CancellationToken>())).ReturnsAsync(customer);
+        
+        return new GetCustomerUseCase(customerRepositoryMock.Object);
+    }
+
+    public static GetCustomerInput CreateInput()
+    {
+        return new GetCustomerInput("Customer 1");
+    }
+}
