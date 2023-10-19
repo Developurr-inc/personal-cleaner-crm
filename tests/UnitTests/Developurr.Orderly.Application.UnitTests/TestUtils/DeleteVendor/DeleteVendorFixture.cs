ae6c43c11593f994f359a6cf5b56977d67ca3298
Dudu Seabra <duduseabra99@gmail.com>
2023-10-19 17:51:50 -0300

    Add delete vendor application tests

---
+++ tests/UnitTests/Developurr.Orderly.Application.UnitTests/TestUtils/DeleteVendor/DeleteVendorFixture.cs
@@ -0,0 +1,27 @@
+using Developurr.Orderly.Application.Command;
+using Developurr.Orderly.Application.Command.Vendor.DeleteVendor;
+using Developurr.Orderly.Domain.Vendor.Repositories;
+using Developurr.Orderly.Domain.UnitTests.TestUtils.Vendor;
+using Moq;
+
+namespace Developurr.Orderly.Application.UnitTests.TestUtils.DeleteVendor;
+
+public static class DeleteVendorFixture
+{
+    public static DeleteVendorUseCase CreateUseCase()
+    {
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
+        var vendorRepositoryMock = new Mock<IVendorRepository>();
+        var input = CreateInput();
+        var vendor = VendorFixture.CreateVendor();
+        
+        vendorRepositoryMock.Setup(x => x.GetByIdAsync(input.VendorId, It.IsAny<CancellationToken>())).ReturnsAsync(vendor);
+        
+        return new DeleteVendorUseCase(unitOfWorkMock.Object, vendorRepositoryMock.Object);
+    }
+
+    public static DeleteVendorInput CreateInput()
+    {
+        return new DeleteVendorInput("Vendor 1");
+    }
+}
