66acf7c18e1e04962bcc55fed30fc490ee4af89c
Guilherme Kneitz <guikneitz@gmail.com>
2023-10-26 17:53:05 -0300

    Done Baby Steps.

---
+++ tests/Developurr.Orderly.API.REST.EndToEndTests/API/Category/CreateCategory/CreateCategory.ApiTest.cs
@@ -0,0 +1,23 @@
+namespace Developurr.Orderly.API.REST.EndToEndTests.API.Category.CreateCategory;
+
+public class CreateCategoryApiTest
+{
+        [Fact(DisplayName = "")]
+        [Trait("EndtoEnd/API", "Category - Endpoints")]
+        public async Task CreateCategory()
+        {
+            //Arrange
+            //var input =CreateCategoryApiTestFixture.getExampleInput();
+
+            //Act
+            //var output = await _fixture().Api
+            //    .Post<CategoryModelOutput>(
+            //    "/categories",
+            //    input
+            //    );
+            
+            //Assert
+            //output.NotNull();
+            
+        }
+}
---
+++ tests/Developurr.Orderly.API.REST.EndToEndTests/API/Category/CreateCategory/CreateCategoryApiTestFixture.cs
@@ -0,0 +1,5 @@
+namespace Developurr.Orderly.API.REST.EndToEndTests.API.Category.CreateCategory;
+public class CreateCategoryApiTestFixture
+{
+
+}
